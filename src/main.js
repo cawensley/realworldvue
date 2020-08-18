24b79c8f98e8476cacc5664898184c961777208e
Alex <cwensley@gmail.com>
2020-08-18 11:45:56 -0600

    added event files

--- src/main.js
+++ src/main.js
@@ -1,7 +1,21 @@
 import Vue from 'vue';
+import upperFirst from 'lodash/upperFirst';
+import camelCase from 'lodash/camelCase';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+
+const requireComponent = require.context(
+  './components',
+  false,
+  /Base[A-Z]\w+\.(vue|js)$/,
+);
+
+requireComponent.keys().forEach((fileName) => {
+  const componentConfig = requireComponent(fileName);
+  const componentName = upperFirst(camelCase(fileName.split('/').pop().replace(/\.\w+$/, '')));
+  Vue.component(componentName, componentConfig.default || componentConfig);
+});
 
 Vue.config.productionTip = false;
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,25 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import Home from '../views/Home.vue';
+import EventList from '../views/EventList.vue';
 
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home,
+    name: 'event-list',
+    component: EventList,
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    path: '/event/:id',
+    name: 'event-show',
+    component: () => import('../views/EventShow.vue'),
+    props: true,
+  },
+  {
+    path: '/event/create',
+    name: 'event-create',
+    component: () => import('../views/EventCreate.vue'),
   },
 ];
 
